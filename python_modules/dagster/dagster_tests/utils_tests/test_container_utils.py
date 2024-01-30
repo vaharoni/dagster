1947b632aef53cdfd46ca4053e95ba7e12d545d1
Chris DeCarolis <cdguitar817@gmail.com>
2024-01-30 15:57:54 -0800

    Account for cgroups v2 in container utilization metrics

--- python_modules/dagster/dagster_tests/utils_tests/test_container_utils.py
+++ python_modules/dagster/dagster_tests/utils_tests/test_container_utils.py
@@ -14,8 +14,13 @@ def temp_dir(tmp_path: Path):
             "DAGSTER_CPU_INFO_PATH": f"{tmp_path}/cpuinfo",
             "DAGSTER_CPU_CFS_QUOTA_US_PATH": f"{tmp_path}/cpu.cfs_quota_us",
             "DAGSTER_CPU_CFS_PERIOD_US_PATH": f"{tmp_path}/cpu.cfs_period_us",
-            "DAGSTER_MEMORY_USAGE_PATH": f"{tmp_path}/memory.usage_in_bytes",
-            "DAGSTER_MEMORY_LIMIT_PATH": f"{tmp_path}/memory.limit_in_bytes",
+            "DAGSTER_MEMORY_USAGE_PATH_V1": f"{tmp_path}/memory.usage_in_bytes",
+            "DAGSTER_MEMORY_LIMIT_PATH_V1": f"{tmp_path}/memory.limit_in_bytes",
+            "DAGSTER_FILESYSTEM_INFO_PATH": f"{tmp_path}/filesystem_info",
+            "DAGSTER_CPU_STAT_PATH": f"{tmp_path}/cpu.stat",
+            "DAGSTER_CPU_MAX_PATH": f"{tmp_path}/cpu.max",
+            "DAGSTER_MEMORY_USAGE_PATH_V2": f"{tmp_path}/memory.current",
+            "DAGSTER_MEMORY_LIMIT_PATH_V2": f"{tmp_path}/memory.max",
         }
     ):
         yield tmp_path
@@ -43,7 +48,7 @@ def temp_dir(tmp_path: Path):
     [True, False],
     ids=["previous-timestamp-set", "previous-timestamp-not-set"],
 )
-def test_containerized_utilization_metrics(
+def test_containerized_utilization_metrics_cgroup_v1(
     temp_dir: str,
     cpu_usage_val: Optional[float],
     cpu_cores_val: Optional[int],
@@ -78,6 +83,9 @@ def test_containerized_utilization_metrics(
         with open(f"{temp_dir}/memory.limit_in_bytes", "w") as f:
             f.write(str(memory_limit_val))
 
+    with open(f"{temp_dir}/filesystem_info", "w") as f:
+        f.write("cpu cgroup: cgroup")
+
     previous_cpu_usage = 1.0 if was_cpu_previously_retrieved else None
     previous_measurement_timestamp = 1.0 if was_previous_timestamp_previously_set else None
     utilization_metrics = retrieve_containerized_utilization_metrics(
@@ -87,8 +95,86 @@ def test_containerized_utilization_metrics(
     )
     assert utilization_metrics["cpu_usage"] == (1.0 if cpu_usage_val else None)
     assert utilization_metrics["num_allocated_cores"] == (1 if cpu_cores_val else None)
+
     assert utilization_metrics["cpu_cfs_quota_us"] == (1.0 if cpu_quota_val else None)
     assert utilization_metrics["cpu_cfs_period_us"] == (1.0 if cpu_period_val else None)
+
+    assert utilization_metrics["memory_usage"] == (1 if memory_usage_val else None)
+    assert utilization_metrics["memory_limit"] == (1 if memory_limit_val else None)
+    assert utilization_metrics["previous_cpu_usage"] == (
+        1.0 if was_cpu_previously_retrieved else None
+    )
+    assert utilization_metrics["previous_measurement_timestamp"] == (
+        1.0 if was_previous_timestamp_previously_set else None
+    )
+
+
+@pytest.mark.parametrize("cpu_usage_val", [1.0, None], ids=["cpu-set", "cpu-not-set"])
+@pytest.mark.parametrize("cpu_cores_val", [1, None], ids=["cpu-cores-set", "cpu-cores-not-set"])
+@pytest.mark.parametrize(
+    "cpu_quota_and_period_val", ["1 1", None], ids=["cpu-max-set", "cpu-max-not-set"]
+)
+@pytest.mark.parametrize(
+    "memory_usage_val", [1, None], ids=["memory-usage-set", "memory-usage-not-set"]
+)
+@pytest.mark.parametrize(
+    "memory_limit_val", [1, None], ids=["memory-limit-set", "memory-limit-not-set"]
+)
+@pytest.mark.parametrize(
+    "was_cpu_previously_retrieved",
+    [True, False],
+    ids=["last-cpu-time-retrieved", "last-cpu-time-not-retrieved"],
+)
+@pytest.mark.parametrize(
+    "was_previous_timestamp_previously_set",
+    [True, False],
+    ids=["previous-timestamp-set", "previous-timestamp-not-set"],
+)
+def test_containerized_utilization_metrics_cgroup_v2(
+    temp_dir: str,
+    cpu_usage_val: Optional[float],
+    cpu_cores_val: Optional[int],
+    cpu_quota_and_period_val: Optional[str],
+    memory_usage_val: Optional[int],
+    memory_limit_val: Optional[int],
+    was_cpu_previously_retrieved: bool,
+    was_previous_timestamp_previously_set: bool,
+):
+    if cpu_usage_val:
+        with open(f"{temp_dir}/cpu.stat", "w") as f:
+            f.write(f"usage_usec {int(cpu_usage_val * 1e6)}")
+
+    if cpu_cores_val:
+        with open(f"{temp_dir}/cpuinfo", "w") as f:
+            f.write("\n".join([f"processor : {i}" for i in range(cpu_cores_val)]))
+
+    if cpu_quota_and_period_val:
+        with open(f"{temp_dir}/cpu.max", "w") as f:
+            f.write(str(cpu_quota_and_period_val))
+
+    if memory_usage_val:
+        with open(f"{temp_dir}/memory.current", "w") as f:
+            f.write(str(memory_usage_val))
+
+    if memory_limit_val:
+        with open(f"{temp_dir}/memory.max", "w") as f:
+            f.write(str(memory_limit_val))
+
+    with open(f"{temp_dir}/filesystem_info", "w") as f:
+        f.write("cpu cgroup: cgroup2")
+
+    previous_cpu_usage = 1.0 if was_cpu_previously_retrieved else None
+    previous_measurement_timestamp = 1.0 if was_previous_timestamp_previously_set else None
+    utilization_metrics = retrieve_containerized_utilization_metrics(
+        logger=None,
+        previous_measurement_timestamp=previous_measurement_timestamp,
+        previous_cpu_usage=previous_cpu_usage,
+    )
+    assert utilization_metrics["cpu_usage"] == (1.0 if cpu_usage_val else None)
+    assert utilization_metrics["num_allocated_cores"] == (1 if cpu_cores_val else None)
+
+    assert utilization_metrics["cpu_cfs_quota_us"] == (1.0 if cpu_quota_and_period_val else None)
+    assert utilization_metrics["cpu_cfs_period_us"] == (1.0 if cpu_quota_and_period_val else None)
 
     assert utilization_metrics["memory_usage"] == (1 if memory_usage_val else None)
     assert utilization_metrics["memory_limit"] == (1 if memory_limit_val else None)
